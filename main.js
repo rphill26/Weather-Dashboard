10db79cbf3383110cc92597610d64a80b49c97c5
Randall Phillips <rphill45@uncc.edu>
2020-01-21 11:45:10 -0500

    trying to get jquery to work

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-$(document).ready(function(){
-  
-  $(".btn").click(function(response){
-    
-    var city = $("#city").val();
-    var APIKey = "&166a433c57516f51dfab1f7edaed8413";
-    
-    if(city != ""){
-      $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?&" +
-        "q=" + city + "&units=imperial&appid=" + APIKey, 
-        type: "GET",
-        datatype: "jsonp",
-        success: function(data){
-          console.log(data);
-        }
-      });
+var city = $("#city").val();
+var queryURL = 'api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=52e1c09c466be469c04de22898ae5113';
 
-    }else{
-      $("#error").html("You didn't add a city!")
-    }
-    
-  });
+const printInfo =(data)=>{
+  console.log(data)
+  let  $h4 = $("<h2>").text(data.name)
+  let icon= `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="weather icon">`
+  $h4.append(icon)
+  const $temp = $("<div>").text("temperature is at: "+ data.main.temp + "F")
+  const $humidity = $("<div>").text("humidty is at: "+ data.main.humidity + "%")
+  const $wind = $("<div>").text("the wind speed: " + data.wind.speed + "mph")
+  $div.append($h4, $temp, $humidity, $wind)
   
+  $("#mainPanel").append($div)
+
+}
+
+$('#submit-weather').click(function(){
+  $.ajax({
+      url: queryURL, 
+      method: "GET",
+      
+    }).then(function(response) {
+      printInfo(response);
+
+    });
   
 });
+
+    
